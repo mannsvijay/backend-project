9dabb9dc77de97e4b76db19a22f5c6e4cba87b2c
mannsvijay <mnvj69@gmail.com>
2026-02-02 23:56:12 +0530

    setup changes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+
+
+
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+    } catch (error) {
+        console.error("ERROR : " ,error)
+        throw err
+        
+    }
+}) ()
