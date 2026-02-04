5cfaf15b9888506db884be35b568efd059b93015
mannsvijay <mnvj69@gmail.com>
2026-02-04 11:13:01 +0530

    some more setup changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path : './env'}) // this gets all environment variables accesible but this isnt professional so we do this
 
-import express from "express";
+import dotenv from "dotenv"; // we prefer this
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+// sec approach includes writing a db connect there and then we just put it into the index file from the index.js file in the db folder so we just need to import it
+
+// connect DB then start server
+connectDB()
+.then(() =>{
+  app.listen(process.env.PORT || 8000 ,() => {
+    console.log(`Server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO db connection failed !!! " , err);
+})
 
 
 
 
-;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
-    } catch (error) {
-        console.error("ERROR : " ,error)
-        throw err
-        
-    }
-}) ()
+
+
+
+
+
+
+
+// FIRST APPROACH to connect to database 
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+//         app.on("error",(error) => {
+//             console.log("ERROR : ",error);
+//             throw error  
+//         })
+
+//         app.listen(process.env.PORT , () => {
+//             console.log(`APP is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR : " ,error)
+//         throw err
+//     }
+// }) ()
