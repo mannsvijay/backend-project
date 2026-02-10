c40395ecf6e41ab612cff010cfcfdd232c3f6c77
mannsvijay <mnvj69@gmail.com>
2026-02-10 11:01:07 +0530

    creating functions for user to register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,41 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/apiError.js";
+import { user } from "../models/user.models.js";
 
 //method to register user
 const registerUser = asyncHandler(async (req,res) => {
-     res.staus(200).json({
-        message : "ok"
-    })
+    //logic to register user
+    //get user details from frontend
+    //validation - not empty
+    //check if user alr exist by username and email
+    //check for images and check for avatar
+    //upload them to cloudinary , avatar
+    // create user object - create entry in db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return response
+
+    const{fullName,email,username,password} = req.body
+
+    // if (fullName === "") {
+    //     throw new ApiError(400,"fullname is required")
+    // }
+
+     if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+
+
+    
+
+ 
+
+    
+    
+    
 })
 
 
