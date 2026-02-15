a9dbb211beadc3a72a9bd4da8a49e09fae90e6fa
mannsvijay <mnvj69@gmail.com>
2026-02-15 13:04:49 +0530

    created middleware  for auth

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,9 +1,26 @@
+import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
-
+import jwt from "jsonwebtoken";
+import { user as User } from "../models/user.models.js";
 
 export const verifyJWT = asyncHandler(async(req,res,next) => {
     // Get the access token from the request header or cookies
-
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+    if(!token){
+        throw new ApiError(401," Unauthorized request, token is missing")
+    }
     
+    // check if the token is valid and not expired using jwt.verify() method
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    if(!user){
+        // todo : discuss about frontend
+        throw new ApiError(401,"invalid access token")
+    }
+
+    req.user = user;
+    next()
 })
