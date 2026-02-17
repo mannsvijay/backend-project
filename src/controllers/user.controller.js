541c87c18eefa73fcaa532efb908d654412646ec
mannsvijay <mnvj69@gmail.com>
2026-02-17 18:04:27 +0530

    feat: add get current user endpoint with success response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -241,13 +241,22 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
 
 })
 
+// get current user
+const getCurrentUser = asyncHandler(async(req,res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
+})
 
 
+//
 
 
 export { 
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser
  };
