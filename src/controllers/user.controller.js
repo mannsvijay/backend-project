29522ccd0ca8e36173d740a33b9b1054be77b05a
mannsvijay <mnvj69@gmail.com>
2026-02-17 17:58:56 +0530

    feat: add change current password endpoint with validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -220,6 +220,31 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req,res) => {
+    const {oldPassword,newPassword} = req.body 
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(401,"password is incorrect")
+    }
+
+    // means password is correct, we can update the password
+    user.password = newPassword
+
+    await user.save({validateBeforeSave : false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , {}, "Password changed successfully"))
+
+})
+
+
+
+
+
 export { 
     registerUser,
     loginUser,
