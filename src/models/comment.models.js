3faf9f173dca3d2294f47f15764afbe711997d28
mannsvijay <mnvj69@gmail.com>
2026-02-24 01:20:45 +0530

    feat: add comment model with schema and pagination support

---
+++ src/models/comment.models.js
@@ -0,0 +1,24 @@
+import mongoose , {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { video } from "./video.models";
+
+const commentSchema =  new Schema ({
+        content : {
+            type : String,
+            required : true,
+        },
+         video :{
+            type : Schema.Types.ObjectId,
+            ref : "video"
+         },
+         owner :{
+            type : Schema.Types.ObjectId,
+            ref : "user"
+         },
+
+
+},{timestamps : true})
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = mongoose.model("comment",commentSchema)
