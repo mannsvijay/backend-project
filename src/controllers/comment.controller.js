dd3741d1c2f8b76443b84a7afbf2926ba0dedceb
mannsvijay <mnvj69@gmail.com>
2026-02-25 13:32:38 +0530

    fixed some bugs

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import {ApiError} from "../utils/apiError.js"
+import {ApiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+    const comments = await Comment.find({video: videoId})
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .populate("user", "username")
+    res.json(new ApiResponse(200, "Comments retrieved successfully", comments))
+
 
 })
 
